e20ac1469393bb3da9d8a1a1f3a8b7341dc2cb53
Alexmacuvel009 <alexoesquecido@gmail.com>
2025-04-16 14:40:11 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -5,11 +5,9 @@ const cors = require('cors');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Conexão com MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,12 +15,10 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('Conectado ao MongoDB com sucesso!'))
 .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
 
-// Rota de teste
 app.get('/', (req, res) => {
   res.send('MozBet backend está online e conectado ao MongoDB!');
 });
 
-// Inicializar servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
